ba6ad0b0e00c54070c815c7c12b3d05cfb42bb54
Olav Vatne <olavvatne@gmail.com>
2015-09-18 19:57:56 +0200

    Removes usage of react-router. No need for react-router so removes that. Remove entry file and renames appHandler and makes a toolTemp instead. The structure should probably change more to better fit our hybrid architecture

--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,23 @@
 import express from "express";
 import React from "react";
-import Router from "react-router";
 import HelloWorld from "../shared/components/HelloWorld";
+import ToolTemp from "../shared/components/ToolTemp";
 const app = express();
 
-// set up Jade
+//TODO: New backend structure that makes more sense.
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
 import routes from "../shared/routes";
 
 app.get('/', function (req, res) {
-    Router.run(routes, req.url, Handler => {
-        let content = React.renderToString(<Handler />);
-        res.render('index', { reactContent: content });
-    });
+    let content = React.renderToString(<ToolTemp />);
+    var templateData = {
+        toolTitle: "Tool #1",
+        toolDescription: "First tool of many.",
+        reactContent: content
+    };
+    res.render('index', templateData);
 });
 
 app.get('/helloworld', function(req, res) {
@@ -23,7 +26,7 @@ app.get('/helloworld', function(req, res) {
         toolTitle: "Tool #1",
         toolDescription: "First tool of many.",
         reactContent: content
-    }
+    };
     res.render('pages/helloworld', templateData);
 });
 
