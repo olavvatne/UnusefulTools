77e895344e11f16331669e463ae0956b88072f82
Torgeir <torgeha@gmail.com>
2015-09-20 16:26:16 +0200

    RGB to hex conversion, validation of input

--- src/client/ColorConverterClient.js
+++ src/client/ColorConverterClient.js
@@ -1,5 +1,6 @@
 
 import React from "react";
+import rgbToHex from "rgb-to-hex";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import mui, { TextField, SelectField, RaisedButton } from 'material-ui';
 import ColorConverter from "../shared/components/ColorConverter";
