f4f69ca1e4c4f9b1a63f350c097726e2f974617a
Olav Vatne <olavvatne@gmail.com>
2015-10-03 01:09:59 +0200

    Fix to style production generation

--- src/server/server.js
+++ src/server/server.js
@@ -53,7 +53,7 @@ if(app.get('env') === 'development') {
 if(app.get('env') === 'production') {
     console.log("==== Render SASS =====")
     sass.render({
-        file: srcPath + 'style/style.scss',
+        file: srcPath + '/style/style.scss',
             outputStyle: 'compressed'
     },
     function(err, result) {
