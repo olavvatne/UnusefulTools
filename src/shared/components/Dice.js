33f62df5160e4141ef244e4f9d665ce286257c98
Torgeir <torgeha@gmail.com>
2015-10-07 14:06:39 +0200

    6-sided die graphics

--- src/shared/components/Dice.js
+++ src/shared/components/Dice.js
@@ -8,17 +8,29 @@ class Dice extends React.Component {
         super();
         this._handleRoll = this._handleRoll.bind(this);
         this._handleAddDie = this._addDie.bind(this);
+        this._generateImagePath = this._getImagePath.bind(this);
         this.defaultEyes = 6;
         this.state = {
             dice: []
         };
     }
 
+    _getImagePath(eyes, number) {
+        return "/images/dice/" + eyes + "-" + number + ".png";
+    }
+
     _addDie() {
         var dice = this.state.dice;
+        var eyes = this.refs.dieEyes.getValue();
+        var number = 1;
+        var imagePath = this._generateImagePath(eyes, number);
+
+        console.log("adding: ", eyes, number, imagePath);
+
         dice.push({
-            eyes: this.refs.dieEyes.getValue(),
-            number: 0
+            eyes: eyes,
+            number: number,
+            imagePath: imagePath
         });
         this.setState({
             dice: dice
@@ -37,9 +49,14 @@ class Dice extends React.Component {
         for (d of dice){
             var number = (Math.floor(Math.random() * d.eyes) + 1);
             //console.log("new die: ", d.eyes, "number: ", number);
+            var eyes = d.eyes;
+
+            var imagePath = this._generateImagePath(eyes, number);
+
             newDice.push({
                 eyes: d.eyes,
-                number: number
+                number: number,
+                imagePath: imagePath
             });
         }
 
@@ -51,29 +68,17 @@ class Dice extends React.Component {
     render() {
 
         var dieEyes = [
-            //{ payload: 3, text: "3"},
             { payload: 4, text: "4"},
-            //{ payload: 5, text: "5"},
             { payload: 6, text: "6"},
-            //{ payload: 7, text: "7"},
             { payload: 8, text: "8"},
-            //{ payload: 9, text: "9"},
             { payload: 10, text: "10"},
-            //{ payload: 11, text: "11"},
             { payload: 12, text: "12"},
-            //{ payload: 13, text: "13"},
-            //{ payload: 14, text: "14"},
-            //{ payload: 15, text: "15"},
-            //{ payload: 16, text: "16"},
-            //{ payload: 17, text: "17"},
-            //{ payload: 18, text: "18"},
-            //{ payload: 19, text: "19"},
             { payload: 20, text: "20"}
         ];
 
         var dice = this.state.dice.map(d => {
             return (
-                <Die number={d.number}/>
+                <Die number={d.number} imagePath={d.imagePath}/>
             );
         })
 
@@ -128,7 +133,7 @@ class Die extends React.Component {
     render() {
         return (
             <div className="dice-row">
-                <h1>{this.props.number}</h1>
+                <img src={this.props.imagePath} alt={this.props.number} width="50" height="50"/>
             </div>
         );
     }
@@ -147,3 +152,5 @@ Dice.toolDescription = "Add dice and roll, as easy as it gets! You can even choo
 Dice.toolMetaDescription = "Online dice roll tool. Can be used with board games like Yatzy, Yahtzee, Dungeons and Dragons and other dice based games. Add dice with appropriate amount of eyes and roll. Available dice includes 4-sided dice, 6-sided dice, 8-sided dice, 10-sided dice, 12-sided dice and 20-sided dice.";
 
 module.exports = Dice;
+
+// <!--<h1>{this.props.number}</h1>-->
